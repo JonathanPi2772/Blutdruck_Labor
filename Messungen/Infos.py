4da6c4d809b8deb739dd6a2581fb56c56fa6668e
JonathanPi2772 <jonathan.pareja2772@gmail.com>
2026-04-07 15:13:27 +0200

    Infos wegen neuer Messungen geupdatet

--- Messungen/Infos.py
+++ Messungen/Infos.py
@@ -82,7 +82,138 @@ MEASUREMENT_INFORMATION: dict = {
     "P08_ACTIV_01_201025": {
         "Beurer": (124, 64),
         "NAIS": (124, 75),
-    }
+    },
+
+    # --- Hanna und Ziyi ---
+    "S001-M001-Rest": {
+        "Beurer": (103, 66),
+        "NAIS": (None, None),
+    },
+    "S001-M001-Active": {
+        "Beurer": (105, 73),
+        "NAIS": (None, None),
+    },
+
+    # ---
+    "S002-M001-Rest": {
+        "Beurer": (169, 107),
+        "NAIS": (155, 104),
+    },
+    "S002-M002-Active": {
+        "Beurer": (152, 110),
+        "NAIS": (170, 111),
+    },
+
+    # ---
+    "S003-M001-Rest": {
+        "Beurer": (107, 64),
+        "NAIS": (111, 73),
+    },
+    "S003-M002-Active": {
+        "Beurer": (110, 66),
+        "NAIS": (115, 75),
+    },
+
+    # ---
+    "S004-M001-Rest": {
+        "Beurer": (131, 82),
+        "NAIS": (122, 82),
+    },
+    "S004-M002-Active": {
+        "Beurer": (126, 85),
+        "NAIS": (122, 81),
+    },
+
+    # ---
+    "S005-M001-Rest": {
+        "Beurer": (129, 96),
+        "NAIS": (147, 101),
+    },
+    "S005-M002-Active": {
+        "Beurer": (146, 95),
+        "NAIS": (148, 103),
+    },
+
+    # ---
+    "S006-M001-Rest": {
+        "Beurer": (134, 87),
+        "NAIS": (133, 83),
+    },
+    "S006-M002-Active": {
+        "Beurer": (125, 89),
+        "NAIS": (159, 122),
+    },
+
+    # ---
+    "S007-M001-Rest": {
+        "Beurer": (134, 89),
+        "NAIS": (165, 102),
+    },
+    "S007-M002-Active": {
+        "Beurer": (149, 89),
+        "NAIS": (180, 88),
+    },
+
+    # ---
+    "S008-M001-Rest": {
+        "Beurer": (137, 94),
+        # "NAIS": (72, 61), # --- Eventuell ignorieren
+        "NAIS": (None, None),
+    },
+    "S008-M002-Active": {
+        "Beurer": (162, 129),
+        "NAIS": (146, 99),
+    },
+
+    # ---
+    "S009-M001-Rest": {
+        "Beurer": (114, 81),
+        "NAIS": (None, None),
+    },
+    "S009-M002-Active": {
+        "Beurer": (115, 81),
+        "NAIS": (132, 97),
+    },
+
+    # ---
+    "S010-M001-Rest": {
+        "Beurer": (108, 75),
+        "NAIS": (127, 80),
+    },
+    "S010-M002-Active": {
+        "Beurer": (124, 76),
+        "NAIS": (132, 87),
+    },
+
+    # ---
+    "S011-M001-Rest": {
+        "Beurer": (133, 90),
+        "NAIS": (139, 104),
+    },
+    "S011-M002-Active": {
+        "Beurer": (144, 92),
+        "NAIS": (150, 96),
+    },
+
+    # ---
+    "S012-M001-Rest": {
+        "Beurer": (136, 87),
+        "NAIS": (128, 89),
+    },
+    "S012-M002-Active": {
+        "Beurer": (140, 82),
+        "NAIS": (139, 84),
+    },
+
+    # ---
+    "S013-M001-Rest": {
+        "Beurer": (89, 74),
+        "NAIS": (None, None),
+    },
+    "S013-M002-Active": {
+        "Beurer": (108, 74),
+        "NAIS": (None, None),
+    },
 }
 
 
